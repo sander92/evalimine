d6d605f927e8dfcd35f577f619d05aabedbc5602
Sander <sander92@ut.ee>
2013-03-11 17:59:12 +0200

    vist tehtud nii, et ainult men[[ uueneb

--- war/sisu.js
+++ war/sisu.js
@@ -3,5 +3,18 @@ function sisu(nimi)
 	
 	$("body").attr("class",nimi);
 
-	$("body").load(nimi+".html");
+	$("#peasisu").load(nimi+".html #sisu");
+
+	//$("#sisu").outerHTML=$("#sisu").innerHTML;
+	//document.getElementById('sisu').outerHTML=document.getElementById('sisu').innerHTML;
+	//}
+	//$("body").load(nimi+".html");
+	
+	/*$.getScript("jquery-latest.js");
+	$.getScript("jquery.tablesorter.js");
+	
+	$.getScript("myTablesorter.js");
+	$.getScript("progress.js");
+	$.getScript("menu.js");
+	$.getScript("sisu.js");*/
 }
