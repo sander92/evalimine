24f596ccfae2b6d4e4b82977d28913cb991d7d6c
Pihel Saatmann <sikakurr@gmail.com>
2013-03-31 23:13:07 +0300

    Fixed hopefully

--- src/evalimised/EvalimisedServlet.java
+++ src/evalimised/EvalimisedServlet.java
@@ -1,10 +1,8 @@
 package evalimised;
 import com.google.appengine.api.rdbms.AppEngineDriver;
 import java.io.IOException;
-import java.io.PrintWriter;
 import java.sql.*;
 import java.util.ArrayList;
-import java.util.Enumeration;
 import java.util.List;
 
 import javax.servlet.http.*;
@@ -14,44 +12,25 @@ import com.google.gson.GsonBuilder;
 
 public class EvalimisedServlet extends HttpServlet{
 
+		
 	public void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
-		Enumeration en = req.getParameterNames();
-	      while (en.hasMoreElements()){
-	          System.out.println(en.nextElement()); 
-	       }
-	  System.out.println();
-	  PrintWriter out = resp.getWriter();
+      String fName = req.getParameter("fName");
+      String lName = req.getParameter("lName");
+      String party = req.getParameter("party");
+      String area = req.getParameter("area");
 	  Connection c = null;
 	    try {
 	      DriverManager.registerDriver(new AppEngineDriver());
 	      c = DriverManager.getConnection("jdbc:google:rdbms://netivalimised2013:netivalimised/evalimised");
-	     // String fName = req.getParameter("firstName");
-	      //String lName = req.getParameter("lastName");
-	      //String party = req.getParameter("party");
-	      //String area = req.getParameter("area");
-		  String fName = "";
-	      String lName = "Lehm";
-	      String party = "";
-	      String area = "";
-	      if(fName != "" || lName !="") {
+
+	      if(fName != "" || lName != "" || party != "" || area != "") {
 	    	  String statement = createQuery(fName,lName,party, area);
 	    	  PreparedStatement stmt = c.prepareStatement(statement);
 		      ResultSet rs = stmt.executeQuery();
-		      List<Candidate> candidates = new ArrayList<Candidate>();
-		      while(rs.next()){
-	                Candidate candidate = new Candidate();
-	                candidate.setFName(rs.getString("FirstName"));
-	                candidate.setLName(rs.getString("LastName"));
-	                candidate.setParty(rs.getString("Party"));
-	                candidate.setArea(rs.getString("Area"));
-	                candidates.add(candidate);
-		      }
-
-		      Gson gson = new GsonBuilder().create();
-	          String categoriesJson = gson.toJson(candidates);
+		      String jsonData = createJSON(rs, party,area);
 	          resp.setContentType("application/json");
 	          resp.setCharacterEncoding("UTF-8");
-	          //resp.getWriter().write(categoriesJson);
+	          resp.getWriter().write(jsonData);
 	      }
 	    } 
 	    catch (SQLException e) {
@@ -68,37 +47,59 @@ public class EvalimisedServlet extends HttpServlet{
 	    //resp.setHeader("Refresh","3; url=/evalimised.jsp");
 	  }
 	
-	private static String createQuery(String fname, String lname, String area, String party) {
+	private static String createQuery(String fname, String lname, String party, String area) {
 		String beginning = "SELECT Person.FirstName AS 'FirstName', Person.LastName AS 'LastName'";
 		String middle = "";
 		String end = "WHERE ";
-		
-		if(fname!="")
+		if(fname!="" && fname != null)
 			end += "FirstName=\""+fname+"\" AND ";	
-		if(lname!="")
+		if(lname!="" && lname != null)
 			end += "LastName=\""+lname+"\" AND ";
-		if(party!="") {
+		if(party!="" && party != null) {
 			beginning += ", Area.AreaName AS 'Area'";
-			middle += "JOIN Party ON Person.PartyID = Party.Party_Id ";
 			end += "PartyID=\""+party+"\" AND ";
 		}
-		if(area!="") {
+		if(area!="" && area != null) {
 			beginning += ", Party.PartyName AS 'Party'";
-			middle += "JOIN Area ON Person.AreaID = Area.Area_Id ";
 			end += "AreaID=\""+area+"\" AND ";
 		}
-		if(party=="" && area==""){
+		if(party=="" && area=="" && party != null && area != null){
 			beginning += ", Area.AreaName AS 'Area'";
 			beginning += ", Party.PartyName AS 'Party'";
-			middle += "JOIN Party ON Person.PartyID = Party.Party_Id ";
-			middle += "JOIN Area ON Person.AreaID = Area.Area_Id ";
+
 		}
+		middle += "JOIN Party ON Person.PartyID = Party.Party_Id ";
+		middle += "JOIN Area ON Person.AreaID = Area.Area_Id ";
 		//remove last " AND "
 		end = end.substring(0, end.length() - 5);
 		beginning +=" FROM Person ";
 		String query = beginning + middle + end;
 		System.out.println(query);
 		return query;
+	}
+	
+	private static String createJSON(ResultSet rs, String party, String area){
+	      List<Candidate> candidates = new ArrayList<Candidate>();
+	      System.out.println(party);
+	      System.out.println(area);
+	      try {
+			while(rs.next()){
+			      Candidate candidate = new Candidate();
+			      candidate.setFName(rs.getString("FirstName"));
+			      candidate.setLName(rs.getString("LastName"));
+			      if(party == "" || party == null)
+			    	  candidate.setParty(rs.getString("Party"));
+			      if(area == "" || area == null)
+			    	  candidate.setArea(rs.getString("Area"));
+			      candidates.add(candidate);
+			  }
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+
+	      Gson gson = new GsonBuilder().create();
+          String candidatesJson = gson.toJson(candidates);
+          return candidatesJson;
 	}
     
 }
